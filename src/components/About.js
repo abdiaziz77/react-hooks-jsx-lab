a7ec33946cef85cfb29d813c381d9e42b2887f74
abdiaziz77 <abdiaziznoor20@gmail.com>
2025-04-11 00:20:50 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React from 'react';
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I am a passionate web developer who loves creating amazing and
+        responsive websites. I enjoy working with JavaScript, React, and other
+        modern technologies.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
